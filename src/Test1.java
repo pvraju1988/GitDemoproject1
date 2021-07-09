1ec380a8d0e56282b95dc8989169c18c7d503679
venkat <venkatrajuj2ee@gmail.com>
2021-07-09 21:33:17 +0530

    First Commit..

---
+++ src/Test1.java
@@ -0,0 +1,11 @@
+
+public class Test1 {
+	public String hello() {
+		return "Hello Git";
+	}
+public static void main(String[] args) {
+	
+	Test1 test1 = new Test1();
+	System.out.println(test1.hello());
+}
+}
